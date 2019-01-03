8a7913fc374868a1ef7df8dfa2722df4dd4f7cd0
Nikolay Mitikov <nmi@sitecore.net>
2019-01-03 14:33:27 +0800

    Initial commit for sqlCommands printer

---
+++ src/Sitecore.LogAnalyzer.MemoryDiagnostics.SqlCommandViewer/ExistingSqlCommandDiBindings.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.LogAnalyzer.MemoryDiagnostics.SqlCommandViewer
+{
+  using System.Data.SqlClient;
+  using Sitecore.LogAnalyzer.MemoryDiagnostics.Connector.DependencyInjection;
+  using Sitecore.MemoryDiagnostics.ClrObjectsProviders;
+  using Sitecore.MemoryDiagnostics.ClrObjectsProviders.Base;
+
+  /// <summary>
+  /// Extracts <see cref="SqlCommand"/> objects from heap and magically prints them!
+  /// </summary>
+  public class ExistingSqlCommandDiBindings : HeapBasedMemoryDumpAnalysisModule
+  { 
+    protected override void RegisterObjectEnumeration()
+    {
+      base.RegisterObjectEnumeration();
+      Rebind<IFilteredObjectsProvider>().To<FilteredObjectProviderByTypeName>().InSingletonScope()
+        .WithConstructorArgument("typeName", typeof(SqlCommand).FullName);      
+    }
+  }
+}
---
+++ src/Sitecore.LogAnalyzer.MemoryDiagnostics.SqlCommandViewer/Invoker.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitecore.LogAnalyzer.MemoryDiagnostics.SqlCommandViewer
+{
+  using System.Drawing;
+  using Sitecore.LogAnalyzer.MemoryDiagnostics.Connector;
+
+  public class Invoker : AbstractDumpInvoker
+  {
+    public override Image Image => Properties.Resources.Picture;
+
+    public override string Name => "SQL commands viewer";
+  }
+}
