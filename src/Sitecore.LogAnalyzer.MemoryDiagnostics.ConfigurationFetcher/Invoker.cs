7edb8c3db2b259ff2fd6d57c6a55650ac95de72b
Nikolay Mitikov <nmi@sitecore.net>
2019-01-03 15:37:37 +0800

    Initial commit of Configuration fetcher

---
+++ src/Sitecore.LogAnalyzer.MemoryDiagnostics.ConfigurationFetcher/Invoker.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.LogAnalyzer.MemoryDiagnostics.ConfigurationFetcher
+{
+  using Sitecore.LogAnalyzer.MemoryDiagnostics.Connector;
+
+  public class Invoker : AbstractDumpInvoker
+  {
+    public override string Name => "Showconfig";
+  }
+}
